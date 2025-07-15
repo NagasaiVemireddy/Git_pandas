f9fdcbbab38e6ebb9766fe2c9d30190aecf37456
NagasaiVemireddy <nagasai.vemireddy999@gmail.com>
2025-07-15 20:06:19 +0530

    Add basic pandas DataFrame example

--- pandas_prac.py
+++ pandas_prac.py
@@ -1,4 +1,27 @@
-
+"""
 print("Testing Git push")
 print("Final test from correct file")
 print("Pushing pandas_prac to GitHub")
+
+"""
+
+#pandas
+
+import pandas as pd
+
+data = {
+    'EmployeeID': [101, 102, 103, 104],
+    'Name': ['Alice', 'Bob', 'Charlie', 'Diana'],
+    'Department': ['HR', 'Finance', 'IT', 'Marketing'],
+    'Salary': [60000, 70000, 80000, 75000]
+}
+
+df = pd.DataFrame(data)
+#display 
+
+print("full data frame ")
+print(df)
+
+
+
+
